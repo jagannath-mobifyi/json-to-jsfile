7e5a2e17026a05f6eb09e909e84d014d2c783afe
jagannath-swarnkar <jagannath18@navgurukul.org>
2021-11-11 18:27:04 +0530

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const express = require('express');
 const cors = require('cors');
-const PORT = 8000;
+const PORT = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json())
